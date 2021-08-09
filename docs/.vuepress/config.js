4a07622c0bfe3dc021930a17e3fd26ebd7854384
sountstars <1298754741@qq.com>
2021-08-09 23:09:37 +0800

    feat:更换icon

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ module.exports = {
   description: "我的个人网站",
   head: [
     // 注入到当前页面的 HTML <head> 中的标签
-    ["link", { rel: "icon", href: "/logo.jpg" }] // 增加一个自定义的 favicon(网页标签的图标)
+    ["link", { rel: "icon", href: "/feature_cat.jpeg" }] // 增加一个自定义的 favicon(网页标签的图标)
   ],
   base: "/", // 这是部署到github相关的配置
   markdown: {
