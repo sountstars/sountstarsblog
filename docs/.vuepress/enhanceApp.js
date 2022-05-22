fd62bd8091df8652eb4c08a4ad968eeaed0cdcd0
liyulai <liyulai@kaikeba.com>
2022-05-22 20:32:35 +0800

    feat: 测试

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,33 +1,33 @@
 export default () => {
   /* 添加水印 */
-//   let app = document.getElementById("app");
-//   let script = document.createElement("script");
-//   script.type = "text/javascript";
-//   console.log(" water-mark");
-//   script.src =
-//     "https://cdn.jsdelivr.net/gh/sountstars/watermark@master/dist/run.js";
-//   app.appendChild(script);
-//   setTimeout(() => {
-//     (() => {
-//       let waterMark = window?.canvansWatermark || function () {};
-//       delete window.waterMark;
-//       waterMark("aurora bolg", null, { left: "10px" });
-//     })();
-//   }, 1000);
+  //   let app = document.getElementById("app");
+  //   let script = document.createElement("script");
+  //   script.type = "text/javascript";
+  //   console.log(" water-mark");
+  //   script.src =
+  //     "https://cdn.jsdelivr.net/gh/sountstars/watermark@master/dist/run.js";
+  //   app.appendChild(script);
+  //   setTimeout(() => {
+  //     (() => {
+  //       let waterMark = window?.canvansWatermark || function () {};
+  //       delete window.waterMark;
+  //       waterMark("aurora bolg", null, { left: "10px" });
+  //     })();
+  //   }, 1000);
 
-let app = !!document && document.getElementById('app')
-let script = !!document && document.createElement("script");
-script.type = "text/javascript";
-script.src = "https://cdn.jsdelivr.net/gh/hzfvictory/cdn@master/water-mark/index.js";
-app.appendChild(script);
+  let app = !!document && document.getElementById("app");
+  let script = !!document && document.createElement("script");
+  script.type = "text/javascript";
+  script.src =
+    "https://cdn.jsdelivr.net/gh/hzfvictory/cdn@master/water-mark/index.js";
+  app.appendChild(script);
 
-setTimeout(() => {
-  (() => {
-    let waterMarkTemp = window?.waterMark || function () {
-    }
-    delete window.waterMark
-    waterMarkTemp('aurora\'s blog', null, 'app', {left: '10px'},)
-  })()
-}, 10000)
-
+  setTimeout(() => {
+    (() => {
+      let waterMarkTemp = window.waterMark || function () {};
+      delete window.waterMark;
+      console.log(" water-mark");
+      waterMarkTemp("aurora's blog", null, "app", { left: "10px" });
+    })();
+  }, 5000);
 };
